26a31cddcf398da62917fe863a47368076581a33
Sergey <SergeyKoznov@MacBook-Pro-Sergey.local>
2014-11-08 22:06:43 +0200

    Initial Commit

---
+++ UITableViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController
+//
+//  Created by Sergey on 11/8/14.
+//  Copyright (c) 2014 com.firmmotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewController
+//
+//  Created by Sergey on 11/8/14.
+//  Copyright (c) 2014 com.firmmotion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
